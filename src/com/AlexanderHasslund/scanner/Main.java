9f160d752ebec16aff6eaa90d8a64bfa7eb1183e
MAxlekansder <144068201+MAxlekansder@users.noreply.github.com>
2023-09-19 11:29:38 +0200

    lagt till nya funktioner

--- src/com/AlexanderHasslund/scanner/Main.java
+++ src/com/AlexanderHasslund/scanner/Main.java
@@ -6,16 +6,40 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
+        //instansierad scanner
+        /*Scanner scan = new Scanner(System.in);
 
-        System.out.println("dwdw");
-        String save = scan.nextLine();
-
-        System.out.println("du skrev: " + save);
+        //System.out.println("dwdw");
+        //String save = scan.nextLine();
+        //System.out.println("du skrev: " + save);
 
         String scan2 = new Scanner(System.in).nextLine();
         System.out.println(scan2);
 
+
+        while(true) {
+            scannerMain();
+            return; // <-- slänger ut oss själva när metoden är klar alt kodstycket
+            //break; <-- blir samma sak som return
+        }
+
+     */
+
+     do {
+         System.out.println("did this run?");
+     } while (false);
+
+
     }
 
+    //helst vill man bryta ut metoder
+    public static void scannerMain() {
+        //static ska inte finnas med om det finns i en annan klass
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Insert some sentence");
+        scan.nextLine();
+
+    }
 }
+
