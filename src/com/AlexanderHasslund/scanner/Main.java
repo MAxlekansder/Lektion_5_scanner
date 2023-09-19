083bbf55f5e4fcf88731d0db032675a7ba696dab
MAxlekansder <144068201+MAxlekansder@users.noreply.github.com>
2023-09-19 10:35:54 +0200

    First commit

---
+++ src/com/AlexanderHasslund/scanner/Main.java
@@ -0,0 +1,21 @@
+package com.AlexanderHasslund.scanner;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("dwdw");
+        String save = scan.nextLine();
+
+        System.out.println("du skrev: " + save);
+
+        String scan2 = new Scanner(System.in).nextLine();
+        System.out.println(scan2);
+
+    }
+
+}
